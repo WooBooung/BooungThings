1d390c01ede50fcddc9142b90a9ddadf0c7f35a4
우성연 <woobooung@gmail.com>
2019-09-08 23:52:46 +0900

    Init Owl Messenger Bot

---
+++ smartapps/woobooung/owl-messenger-bot.src/owl-messenger-bot.groovy
@@ -0,0 +1,299 @@
+/*
+ *  Owl Messenger Bot
+ *
+ *
+ *  Copyright 2019 WooBooung <woobooung@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Version history
+*/
+public static String version() { return "v0.0.1.20190908" }
+/*
+ *	2019/09/08 >>> v0.0.1.20190908 - Init App
+  */
+definition(
+	name: "Owl Messenger Bot",
+	namespace: "WooBooung",
+	author: "Booung",
+	description: "Send a message through your messenger bot when the mode changes or other events occur.",
+	category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/mail_contact.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/mail_contact@2x.png"
+)
+
+preferences {
+	page(name: "mainPage", title: "Send a message on your Massenger Bot when something happens", install: true, uninstall: true)
+	page(name: "timeIntervalInput", title: "Only during a certain time") {
+		section {
+			input "starting", "time", title: "Starting", required: false
+			input "ending", "time", title: "Ending", required: false
+		}
+	}
+}
+
+def mainPage() {
+	dynamicPage(name: "mainPage") {
+    	section{
+			input "message","text",title:"Send this message", required:true, multiple: false
+		}
+		section {
+			input "messengerBot", "capability.speechSynthesis", title: "On this Messenger Bot", required: true, multiple: true
+		}
+		def anythingSet = anythingSet()
+		if (anythingSet) {
+			section("Play message when"){
+				ifSet "motion", "capability.motionSensor", title: "Motion Here", required: false, multiple: true
+				ifSet "contact", "capability.contactSensor", title: "Contact Opens", required: false, multiple: true
+				ifSet "contactClosed", "capability.contactSensor", title: "Contact Closes", required: false, multiple: true
+				ifSet "acceleration", "capability.accelerationSensor", title: "Acceleration Detected", required: false, multiple: true
+				ifSet "mySwitch", "capability.switch", title: "Switch Turned On", required: false, multiple: true
+				ifSet "mySwitchOff", "capability.switch", title: "Switch Turned Off", required: false, multiple: true
+				ifSet "arrivalPresence", "capability.presenceSensor", title: "Arrival Of", required: false, multiple: true
+				ifSet "departurePresence", "capability.presenceSensor", title: "Departure Of", required: false, multiple: true
+				ifSet "smoke", "capability.smokeDetector", title: "Smoke Detected", required: false, multiple: true
+				ifSet "water", "capability.waterSensor", title: "Water Sensor Wet", required: false, multiple: true
+				ifSet "button1", "capability.button", title: "Button Press", required:false, multiple:true 
+				ifSet "triggerModes", "mode", title: "System Changes Mode", required: false, multiple: true
+				ifSet "timeOfDay", "time", title: "At a Scheduled Time", required: false
+			}
+		}
+		def hideable = anythingSet || app.installationState == "COMPLETE"
+		def sectionTitle = anythingSet ? "Select additional triggers" : "Send message when..."
+		section(sectionTitle, hideable: hideable, hidden: true){
+			ifUnset "motion", "capability.motionSensor", title: "Motion Here", required: false, multiple: true
+			ifUnset "contact", "capability.contactSensor", title: "Contact Opens", required: false, multiple: true
+			ifUnset "contactClosed", "capability.contactSensor", title: "Contact Closes", required: false, multiple: true
+			ifUnset "acceleration", "capability.accelerationSensor", title: "Acceleration Detected", required: false, multiple: true
+			ifUnset "mySwitch", "capability.switch", title: "Switch Turned On", required: false, multiple: true
+			ifUnset "mySwitchOff", "capability.switch", title: "Switch Turned Off", required: false, multiple: true
+			ifUnset "arrivalPresence", "capability.presenceSensor", title: "Arrival Of", required: false, multiple: true
+			ifUnset "departurePresence", "capability.presenceSensor", title: "Departure Of", required: false, multiple: true
+			ifUnset "smoke", "capability.smokeDetector", title: "Smoke Detected", required: false, multiple: true
+			ifUnset "water", "capability.waterSensor", title: "Water Sensor Wet", required: false, multiple: true
+			ifUnset "button1", "capability.button", title: "Button Press", required:false, multiple:true //remove from production
+			ifUnset "triggerModes", "mode", title: "System Changes Mode", description: "Select mode(s)", required: false, multiple: true
+			ifUnset "timeOfDay", "time", title: "At a Scheduled Time", required: false
+		}
+		section("More options", hideable: true, hidden: true) {
+			input "frequency", "decimal", title: "Minimum time between actions (defaults to every event)", description: "Minutes", required: false
+			href "timeIntervalInput", title: "Only during a certain time", description: timeLabel ?: "Tap to set", state: timeLabel ? "complete" : "incomplete"
+			input "days", "enum", title: "Only on certain days of the week", multiple: true, required: false,
+				options: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+			if (settings.modes) {
+            	input "modes", "mode", title: "Only when mode is", multiple: true, required: false
+            }
+			input "oncePerDay", "bool", title: "Only once per day", required: false, defaultValue: false
+		}
+
+        section([mobileOnly:true]) {
+            mode title: "Set for specific mode(s)", required: false
+        }
+        
+        section("About"){
+            paragraph "${version()}"
+        }
+	}
+}
+
+private anythingSet() {
+	for (name in ["motion","contact","contactClosed","acceleration","mySwitch","mySwitchOff","arrivalPresence","departurePresence","smoke","water","button1","timeOfDay","triggerModes","timeOfDay"]) {
+		if (settings[name]) {
+			return true
+		}
+	}
+	return false
+}
+
+private ifUnset(Map options, String name, String capability) {
+	if (!settings[name]) {
+		input(options, name, capability)
+	}
+}
+
+private ifSet(Map options, String name, String capability) {
+	if (settings[name]) {
+		input(options, name, capability)
+	}
+}
+
+private updateLabel() {
+	app.updateLabel("To bot: " + settings.message)
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+    updateLabel()
+    subscribeToEvents()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+    updateLabel()
+    unsubscribe()
+	unschedule()
+	subscribeToEvents()
+}
+
+def subscribeToEvents() {
+	subscribe(app, appTouchHandler)
+	subscribe(contact, "contact.open", eventHandler)
+	subscribe(contactClosed, "contact.closed", eventHandler)
+	subscribe(acceleration, "acceleration.active", eventHandler)
+	subscribe(motion, "motion.active", eventHandler)
+	subscribe(mySwitch, "switch.on", eventHandler)
+	subscribe(mySwitchOff, "switch.off", eventHandler)
+	subscribe(arrivalPresence, "presence.present", eventHandler)
+	subscribe(departurePresence, "presence.not present", eventHandler)
+	subscribe(smoke, "smoke.detected", eventHandler)
+	subscribe(smoke, "smoke.tested", eventHandler)
+	subscribe(smoke, "carbonMonoxide.detected", eventHandler)
+	subscribe(water, "water.wet", eventHandler)
+	subscribe(button1, "button.pushed", eventHandler)
+
+	if (triggerModes) {
+		subscribe(location, modeChangeHandler)
+	}
+
+	if (timeOfDay) {
+		schedule(timeOfDay, scheduledTimeHandler)
+	}
+}
+
+def eventHandler(evt) {
+	log.trace "eventHandler($evt?.name: $evt?.value)"
+	if (allOk) {
+		log.trace "allOk"
+		def lastTime = state[frequencyKey(evt)]
+		if (oncePerDayOk(lastTime)) {
+			if (frequency) {
+				if (lastTime == null || now() - lastTime >= frequency * 60000) {
+					takeAction(evt)
+				}
+				else {
+					log.debug "Not taking action because $frequency minutes have not elapsed since last action"
+				}
+			}
+			else {
+				takeAction(evt)
+			}
+		}
+		else {
+			log.debug "Not taking action because it was already taken today"
+		}
+	}
+}
+
+def modeChangeHandler(evt) {
+	log.trace "modeChangeHandler $evt.name: $evt.value ($triggerModes)"
+	if (evt.value in triggerModes) {
+		eventHandler(evt)
+	}
+}
+
+def scheduledTimeHandler() {
+	eventHandler(null)
+}
+
+def appTouchHandler(evt) {
+	takeAction(evt)
+}
+
+private takeAction(evt) {
+	log.trace "takeAction() ${settings.message}"
+	
+    messengerBot.speak(settings.message)
+
+	if (frequency || oncePerDay) {
+		state[frequencyKey(evt)] = now()
+	}
+	log.trace "Exiting takeAction()"
+}
+
+private frequencyKey(evt) {
+	"lastActionTimeStamp"
+}
+
+private dayString(Date date) {
+	def df = new java.text.SimpleDateFormat("yyyy-MM-dd")
+	if (location.timeZone) {
+		df.setTimeZone(location.timeZone)
+	}
+	else {
+		df.setTimeZone(TimeZone.getTimeZone("America/New_York"))
+	}
+	df.format(date)
+}
+
+private oncePerDayOk(Long lastTime) {
+	def result = true
+	if (oncePerDay) {
+		result = lastTime ? dayString(new Date()) != dayString(new Date(lastTime)) : true
+		log.trace "oncePerDayOk = $result"
+	}
+	result
+}
+
+// TODO - centralize somehow
+private getAllOk() {
+	modeOk && daysOk && timeOk
+}
+
+private getModeOk() {
+	def result = !modes || modes.contains(location.mode)
+	log.trace "modeOk = $result"
+	result
+}
+
+private getDaysOk() {
+	def result = true
+	if (days) {
+		def df = new java.text.SimpleDateFormat("EEEE")
+		if (location.timeZone) {
+			df.setTimeZone(location.timeZone)
+		}
+		else {
+			df.setTimeZone(TimeZone.getTimeZone("America/New_York"))
+		}
+		def day = df.format(new Date())
+		result = days.contains(day)
+	}
+	log.trace "daysOk = $result"
+	result
+}
+
+private getTimeOk() {
+	def result = true
+	if (starting && ending) {
+		def currTime = now()
+		def start = timeToday(starting, location?.timeZone).time
+		def stop = timeToday(ending, location?.timeZone).time
+		result = start < stop ? currTime >= start && currTime <= stop : currTime <= stop || currTime >= start
+	}
+	log.trace "timeOk = $result"
+	result
+}
+
+private hhmm(time, fmt = "h:mm a")
+{
+	def t = timeToday(time, location.timeZone)
+	def f = new java.text.SimpleDateFormat(fmt)
+	f.setTimeZone(location.timeZone ?: timeZone(time))
+	f.format(t)
+}
+
+private getTimeLabel()
+{
+	(starting && ending) ? hhmm(starting) + "-" + hhmm(ending, "h:mm a z") : ""
+}
